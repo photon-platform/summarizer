0012063c093e9c110bf2a2b832cf7faef003c0cc
phiarchitect <phi@phiarchitect.com>
2023-04-15 15:55:01 -0700

    test youtube

--- captions.sh
+++ captions.sh
@@ -4,7 +4,16 @@ VIDEO_ID=uu3NzgnAiMk
 
 URL="https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=$GOOGLE_API_KEY&part=snippet,contentDetails,statistics,status"
 
+# echo $URL
+# echo
+# wget $URL -O details.json
+# cat details.json
+
+URL="https://www.googleapis.com/youtube/v3/captions?part=snippet&videoId=$VIDEO_ID&key=$GOOGLE_API_KEY"
+# URL="https://www.googleapis.com/youtube/v3/captions/$VIDEO_ID?part=snippet&key=$GOOGLE_API_KEY"
+# URL="https://www.googleapis.com/youtube/v3/captions/$VIDEO_ID"
+
 echo $URL
 echo
-wget $URL
-
+wget $URL -O captions.json
+cat captions.json
