5833c87b56e235eaf8eea74a8e055d315522d817
phiarchitect <phi@phiarchitect.com>
2023-05-21 10:02:01 -0700

    add pyproject.toml - move to src

---
+++ src/photon_platform/summarizer/__init__.py
@@ -0,0 +1,16 @@
+"""
+summarizer
+==========
+
+PHOTON platform
+---------------
+
+tools for gathering and summarizing web knowledge
+
+"""
+
+__author__ = "phiarchitect"
+__maintainer__ = "PHOTON platform"
+__email__ = "github@phiarchitect.com"
+__version__ = "0.0.1"
+__licence__ = "MIT"
